8ad4193682cfe9514833fadd445329ace9206605
Fawwaz Ibraheem <fawazpro27@gmail.com>
2021-09-09 17:51:17 +0100

    timer

--- ci/app/Views/page.php
+++ ci/app/Views/page.php
@@ -1,9 +1,9 @@
-
 <?php
-function valid($txt){
-    if(empty($txt)){
+function valid($txt)
+{
+    if (empty($txt)) {
         return 'N/A';
-    }else{
+    } else {
         return $txt;
     }
 }
@@ -61,19 +61,19 @@ function price(int $pri)
     <div class="sc-drMfKT VhNRA">
         <div class="sc-fgfRvd eDMjsG">
             <div class="sc-hIVACf jsbDhJ">
-                <div class="logo-name"><img class="logo" src="<?=$pool['image_url']?>" alt="logo">
+                <div class="logo-name"><img class="logo" src="<?= $pool['image_url'] ?>" alt="logo">
                     <div class="name-and-links">
                         <div class="name-logo">
-                            <h2><?=$pool['name']?></h2>
+                            <h2><?= $pool['name'] ?></h2>
                         </div>
-                        <div class="social-links"><a href=<?=$pool['url']?>"><?=$pool['url']?></a></div>
+                        <div class="social-links"><a href=<?= $pool['url'] ?>"><?= $pool['url'] ?></a></div>
                     </div>
                     <div class="btn-block"><a onclick="toggle1()" class="sc-kEYyzF sc-gpHHfC jjTphu"><span>Join
                                 Whitelist</span></a></div>
                 </div>
                 <div class="sc-gVyKpa dRqDnJ">
                     <h3 class="overview">Project Overview</h3>
-                    <div class="description"><?=$pool['description']?></div>
+                    <div class="description"><?= $pool['description'] ?></div>
                     <h3 class="overview">Pool Details</h3>
                     <div class="information">
                         <p>Pool Information</p>
@@ -84,11 +84,11 @@ function price(int $pri)
                             </div> -->
                             <div>
                                 <hr>
-                                <div class="css-vurnku">Access Type<span><?=$pool['access_type']?></span></div>
+                                <div class="css-vurnku">Access Type<span><?= $pool['access_type'] ?></span></div>
                             </div>
                             <div>
                                 <hr>
-                                <div class="css-vurnku">Token Distribution<span><?=valid($pool['token_distrib'])?></span></div>
+                                <div class="css-vurnku">Token Distribution<span><?= valid($pool['token_distrib']) ?></span></div>
                             </div>
                         </div>
                     </div>
@@ -97,33 +97,118 @@ function price(int $pri)
                         <div class="table">
                             <div>
                                 <hr>
-                                <div class="css-vurnku">Symbols<span><?=valid($pool['symbol'])?></span></div>
+                                <div class="css-vurnku">Symbols<span><?= valid($pool['symbol']) ?></span></div>
                             </div>
                             <div>
                                 <hr>
-                                <div class="css-vurnku">Token Price<span><?=empty($pool['token_price'])? 'N/A' : '$'.$pool['token_price']?></span></div>
+                                <div class="css-vurnku">Token Price<span><?= empty($pool['token_price']) ? 'N/A' : '$' . $pool['token_price'] ?></span></div>
                             </div>
                             <div>
                                 <hr>
-                                <div class="css-vurnku">Total Supply<span><?=valid(price($pool['total_supply']))?></span></div>
+                                <div class="css-vurnku">Total Supply<span><?= valid(price($pool['total_supply'])) ?></span></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="sc-eXNvrr ihPGuK">
-                <?php if($pool['status'] == 'filled'): ?>
-                <h3 class="sale-starts-in">Sale Has Ended</h3>
-                <?php elseif($pool['status'] == 'upcoming'): ?>
-                <h3 class="sale-starts-in">Sales Starts in <br> <?=rand(1,19)?> <br> days</h3>
+                <?php if ($pool['status'] == 'filled') : ?>
+                    <h3 class="sale-starts-in">Sale Has Ended</h3>
+                <?php elseif ($pool['status'] == 'upcoming') : ?>
+                    <h3 class="sale-starts-in">Sales Starts in </h3>
+                    <div class="sc-bEjcJn lntlBq">
+                        <div class="sc-ePZHVD eytwNi">
+                            <p class="number" id="days">-</p>
+                            <p class="meter">Days</p>
+                        </div>
+                        <div class="sc-ePZHVD eytwNi">
+                            <p class="number" id="hours">-</p>
+                            <p class="meter">Hours</p>
+                        </div>
+                        <div class="sc-ePZHVD eytwNi">
+                            <p class="number" id="mins">-</p>
+                            <p class="meter">Minutes</p>
+                        </div>
+                        <div class="sc-ePZHVD eytwNi">
+                            <p class="number" id="secs">-</p>
+                            <p class="meter">Seconds</p>
+                        </div>
+                    </div>
+
+                    <style>
+                        .lntlBq {
+                            display: flex;
+                            -webkit-box-pack: justify;
+                            justify-content: space-between;
+                            width: 100%;
+                        }
+
+                        .eytwNi {
+                            background-color: rgb(42, 44, 50);
+                            border: 1px solid rgb(63, 62, 62);
+                            border-radius: 5px;
+                            width: 90px;
+                            height: 72px;
+                            margin: 0px 5px;
+                        }
+
+                        .eytwNi .number {
+                            font-family: Montserrat;
+                            font-style: normal;
+                            font-weight: 600;
+                            font-size: 25px;
+                            line-height: 20px;
+                            text-align: center;
+                            margin: 15px 0px 0px;
+                        }
+
+                        .eytwNi .meter {
+                            font-family: Montserrat;
+                            font-style: normal;
+                            font-weight: 600;
+                            font-size: 13px;
+                            line-height: 20px;
+                            text-align: center;
+                            margin: 10px 0px 0px;
+                            color: rgb(143, 144, 149);
+                        }
+                    </style>
+                    <script src="https://cdn.jsdelivr.net/npm/easytimer@1.1.3/src/easytimer.min.js"></script>
+                    <script>
+                        var timer = new Timer();
+                        timer.start({
+                            countdown: true,
+                            startValues: {
+                                days: 3,
+                                hours: 5,
+                                minutes: 1,
+                                seconds: 5
+                            }
+                        });
+
+                        // document.querySelector('#countdownExample .values').innerHTML= timer.getTimeValues().toString();
+
+                        timer.addEventListener('secondsUpdated', function(e) {
+                            document.querySelector('#days').innerHTML= timer.getTimeValues().days.toString();
+                            document.querySelector('#hours').innerHTML= timer.getTimeValues().hours.toString();
+                            document.querySelector('#mins').innerHTML= timer.getTimeValues().minutes.toString();
+                            document.querySelector('#secs').innerHTML= timer.getTimeValues().seconds.toString();
+                        });
+
+                        timer.addEventListener('targetAchieved', function(e) {
+                            document.querySelector('#countdownExample .values').innerHTML = 'Ended';
+                        });
+                    </script>
+
+
                 <?php endif; ?>
                 <div class="sc-cpmKsF iZxBap">
                     <div>
                         <p>Swap Ratio</p>
-                        <h4>1 ETH = <?= empty($pool['swap_rate']) ? 'TBA' : $pool['swap_rate']?></h4>
+                        <h4>1 ETH = <?= empty($pool['swap_rate']) ? 'TBA' : $pool['swap_rate'] ?></h4>
                     </div>
                 </div><button onclick="toggle1()" class="sc-kEYyzF sc-gPzReC jGlXEG"><span>Connect Wallet</span></button>
-                <div class="css-1436xfi">Progress</div><progress class="sc-kQsIoO iCjQwl multi-progress" value="<?=empty($pool['progress'])? '100' : $pool['progress']?>" max="100"></progress>
+                <div class="css-1436xfi">Progress</div><progress class="sc-kQsIoO iCjQwl multi-progress" value="<?= empty($pool['progress']) ? '100' : $pool['progress'] ?>" max="100"></progress>
             </div>
         </div>
     </div>
